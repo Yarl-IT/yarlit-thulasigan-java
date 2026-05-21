f2075b1f4a7280e18836e4f6f944ca8135b2e62c
Thulasigan Nagalingam <nstthulasigan@gmail.com>
2026-05-21 16:46:54 +0530

    Create PrintDemo.java

---
+++ Java_basic/TASK_02/PrintDemo.java
@@ -0,0 +1,15 @@
+class PrintDemo{
+	public static void main(String args[]){
+		System.out.println("My name is A");
+		System.out.println("My father's name is B");
+		System.out.println("My age is 20");
+		System.out.println( );
+		System.out.print("My name is A");
+		System.out.print("My father's name is B");
+		System.out.print("My age is 20");
+		System.out.println( );
+		System.out.print("My name is A\n");
+		System.out.print("My father's name is B\n");
+		System.out.print("My age is 20\n");
+	}
+}
