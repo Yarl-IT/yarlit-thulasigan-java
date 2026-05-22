b8ea530abd7985f4aca4c00639d9648ac0a98865
Thulasigan Nagalingam <nstthulasigan@gmail.com>
2026-05-22 10:19:31 +0530

    Create If_Marks.java

---
+++ Java_basic/TASK_15/If_Marks.java
@@ -0,0 +1,11 @@
+class If_Marks{
+	public static void main(String args[]){
+		int student_mark=55;
+		
+		if (student_mark>=50){
+			System.out.println("Pass");
+		}else{
+			System.out.println("Fail");
+		}
+	}
+}
