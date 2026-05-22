a27788d799cac9cfb13b531e4c040b7e9e3cfcff
Thulasigan Nagalingam <nstthulasigan@gmail.com>
2026-05-22 08:58:06 +0530

    Create Variable3.java

---
+++ Java_basic/TASK_05/Variable3.java
@@ -0,0 +1,8 @@
+class Varable3{
+	public static void main(String args[]){
+		String fname,lname;	//=is a assignment operator
+		fname="Thulasigan";
+		lname="Nagalingam";
+		System.out.println(fname+lname);
+	}
+}
