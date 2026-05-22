e405751b1ec58d61cc5d91da827430087725c226
Thulasigan Nagalingam <nstthulasigan@gmail.com>
2026-05-22 09:25:33 +0530

    Update Variable3.java

--- Java_basic/TASK_05/Variable3.java
+++ Java_basic/TASK_05/Variable3.java
@@ -1,8 +1,10 @@
-class Varable3{
+class Variable3{
 	public static void main(String args[]){
 		String fname,lname;	//=is a assignment operator
 		fname="Thulasigan";
 		lname="Nagalingam";
-		System.out.println(fname+lname);
+		System.out.println("First name:"+fname);
+		System.out.println("Last name :"+lname);
+		System.out.println("Full name :"+fname+" "+lname);
 	}
 }
