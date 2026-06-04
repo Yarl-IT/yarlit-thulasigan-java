acd58068e9ffa9458d2b1ce51afd69ff35be2bf2
Thulasigan Nagalingam <nstthulasigan@gmail.com>
2026-06-04 10:11:42 +0530

    Create ArrayWhileLoopForward.java

---
+++ Java_basic/TASK_30/ArrayWhileLoopForward.java
@@ -0,0 +1,10 @@
+class ArrayWhileLoopForward{
+	public static void main(String args[]){
+		int[] y={10,20,30,40,50};
+		int x=0;
+		while (x<y.length){
+			System.out.println(y[x]);
+			x++;
+		}
+	}
+}
