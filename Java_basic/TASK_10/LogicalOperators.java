0a1140e327cf9a1ccd6e14bd91e2519d988b8d5e
Thulasigan Nagalingam <nstthulasigan@gmail.com>
2026-06-17 14:04:15 +0530

    delete

--- Java_basic/TASK_10/LogicalOperators.java
+++
@@ -1,9 +0,0 @@
-class LogicalOperators{
-	public static void main(String args[]){
-		boolean a=true,b=false;
-		
-		System.out.println("a&&b: "+(a&&b)); //AND
-		System.out.println("a||b: "+(a||b)); //OR
-		System.out.println("!a:   "+!a); //NOT
-	}
-}
