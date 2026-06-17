c60c3dd669042180b12b3b3b1f637613a2322201
Thulasigan Nagalingam <nstthulasigan@gmail.com>
2026-06-17 13:54:17 +0530

    Create Inverted_Right_Angled_Star_Triangle.java

---
+++ TASK_55/Inverted_Right_Angled_Star_Triangle.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Inverted_Right_Angled_Star_Triangle{
+	public static void main(String args[]){
+		Scanner scan=new Scanner(System.in);
+		System.out.print("How many rows do you want?   ");
+		int row=scan.nextInt();
+		for(int x=row;x>0;x--){
+			for(int y=1;y<=x;y++){
+				System.out.print("* ");
+			}
+			System.out.println("");
+		}
+	}
+}
